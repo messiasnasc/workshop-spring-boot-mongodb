08c90441d3111c144548601276882b1a8841ca3c
Messias Nascimento <messiasjogos15@gmail.com>
2024-09-21 19:18:00 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/messiasnasc/workshopmongo/resources/UserResource.java
+++ src/main/java/com/messiasnasc/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.messiasnasc.workshopmongo.domain.Post;
 import com.messiasnasc.workshopmongo.domain.User;
 import com.messiasnasc.workshopmongo.dto.UserDTO;
 import com.messiasnasc.workshopmongo.services.UserService;
@@ -59,4 +60,9 @@ public class UserResource {
 	    obj = service.update(obj);
 	    return ResponseEntity.noContent().build();
 	}
+	@GetMapping(value = "/{id}/posts")
+ 	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+}
 }
